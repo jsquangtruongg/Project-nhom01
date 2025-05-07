b14c7a886412e1168dfcee1bc07c94f36ef71ee5
jstruongg <nguyenqtthangbinh@gmail.com>
2025-05-07 19:51:35 +0700

    your message

---
+++ src/components/client/home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const homeComponent = () => {
+  return (
+    <div className="fom-header">
+      <div className="from">ddđ</div>
+    </div>
+  );
+};
+export default homeComponent;
